de13c20461d48b4379fc73b172a5dc0f4302e03a
rinduaurellia <rindu.aurellia@ui.ac.id>
2025-09-17 15:37:40 +0700

    TUTORIAL 3 SELESAI

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from main.views import show_main, create_news, show_news
-from main.views import show_main, create_news, show_news, show_xml
-from main.views import show_main, create_news, show_news, show_xml, show_json
 from main.views import show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -14,4 +14,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
