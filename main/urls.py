9be480bc40761362e62759e10ac8f5f99950a2fd
rinduaurellia <rindu.aurellia@ui.ac.id>
2025-10-01 14:51:17 +0700

    TUTORIAL 5 SELESAI

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import edit_news
 from main.views import delete_news
+from main.views import add_news_entry_ajax
+
 
 app_name = 'main'
 
@@ -21,6 +23,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('news/<uuid:id>/edit', edit_news, name='edit_news'),
     path('news/<uuid:id>/delete', delete_news, name='delete_news'),
-
+    path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
 
 ]
